678074dfc8dbc6947911b8e126259f746dd6c204
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-11-06 10:04:09 +0100

    Aggiunto icona

--- Notepad/FormMain.Designer.cs
+++ Notepad/FormMain.Designer.cs
@@ -432,6 +432,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.topMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.topMenu;
             this.Name = "FormMain";
             this.Text = "Blocco note di Windows";
